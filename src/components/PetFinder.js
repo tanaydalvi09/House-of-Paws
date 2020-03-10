2e13d3178d7f8dbc3cbff2479c7746d392422b23
Shraddha <SP@ps-MacBook-Pro.fritz.box>
2020-03-10 16:29:50 +0100

    PetFinder changes

--- src/components/PetFinder.js
+++ src/components/PetFinder.js
@@ -8,22 +8,98 @@ import React, {Component} from 'react';
 
 
 class PetFinder extends Component{
-  
 
+//     componentDidMount(){
+//         const M = window.M;
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.pagination');
+    //var elem = document.querySelectorAll('.carousel');
+   //  var instances = M.Pagination.init(elems, {});
+   //var instance = M.Carousel.init(elem, {});
+ // });
+ 
+    //}
+    componentDidMount(){
+     const M = window.M;
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.dropdown-trigger');
+        var options = document.querySelectorAll('.alignment')
+        var instances = M.Dropdown.init(elems, options);
+      });
+
+    }
   
     render(){
     return(
+        <div>
+
 <div class="row">
-<div class="col s3">
+<div class="col s2">
  <i class="material-icons">arrow_back</i>
+ <h7>Back to Home</h7>
  </div>
- <div class="col s3">
- <h5>Back to Home</h5>
+ 
+ </div>
+
+ <div class="row3">
+ <div class="col s12">
+ <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Distance</a>
+ <ul id='dropdown1' class='dropdown-content'>
+    <li><a href="#!">one</a></li>
+    <li><a href="#!">two</a></li>
+    <li class="divider" tabindex="-1"></li>
+    <li><a href="#!">three</a></li>
+    <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+    
+  </ul>
+  <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Distance</a>
+ <ul id='dropdown1' class='dropdown-content'>
+    <li><a href="#!">one</a></li>
+    <li><a href="#!">two</a></li>
+    <li class="divider" tabindex="-1"></li>
+    <li><a href="#!">three</a></li>
+    <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+  </ul>
  </div>
  </div>
 
+ 
+     <div class="row">
+     <div class="col8">
+     <ul class="pagination">
+<li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+<li class="active"><a href="#!">1</a></li>
+<li class="waves-effect"><a href="#!">2</a></li>
+<li class="waves-effect"><a href="#!">3</a></li>
+<li class="waves-effect"><a href="#!">4</a></li>
+<li class="waves-effect"><a href="#!">5</a></li>
+<li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+</ul>
+</div>
+</div>
+</div>
+
+ 
+ 
+
+/* <div class="pagination">
+<ul class="pagination">
+<li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+<li class="active"><a href="#!">1</a></li>
+<li class="waves-effect"><a href="#!">2</a></li>
+<li class="waves-effect"><a href="#!">3</a></li>
+<li class="waves-effect"><a href="#!">4</a></li>
+<li class="waves-effect"><a href="#!">5</a></li>
+<li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+</ul>
+</div> */
 
     )
     }
+
+    
+    
 }
 export default PetFinder
